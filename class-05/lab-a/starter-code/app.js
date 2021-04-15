3ea6bc281492d94dd4ff6014d2d4a4d7862a7cab
shahd <shahd.jm33@gmail.com>
2021-04-15 15:48:11 +0300

    Solve the first problem

--- class-05/lab-a/starter-code/app.js
+++ class-05/lab-a/starter-code/app.js
@@ -8,12 +8,13 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
+
 function sum(a, b) { //eslint-disable-line
   let sum = a + b;
   let msg = 'The sum of '+ a +' and '+ b +' is '+ sum +'.';
   return [sum, msg]
 }
-// console.log( sum(4, 11));
+  console.log( sum(4, 11));
 
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
